0375a1a811b5e8c68859a900c701c4acf44a6cd7
Bob Raijmakers <bob.raijmakers@luminis.eu>
2022-12-02 15:27:40 +0100

    2022 - day2

--- 2022/src/main/java/Main.java
+++ 2022/src/main/java/Main.java
@@ -1,12 +1,13 @@
 package main.java;
 
 import main.java.days.Day1;
+import main.java.days.Day2;
 
 public class Main {
     public static void main(String[] args) {
-        Day1 day1 = new Day1();
-        String answer1 = day1.getAnswer(1);
-        String answer2 = day1.getAnswer(2);
+        Day day = new Day2();
+        String answer1 = day.getAnswer(Day.Part.ONE);
+        String answer2 = day.getAnswer(Day.Part.TWO);
         System.out.println(answer1);
         System.out.println(answer2);
     }
---
+++ 2022/src/main/java/days/Day2.java
@@ -0,0 +1,102 @@
+package main.java.days;
+
+import main.java.Day;
+import main.java.util.FilesUtil;
+
+import java.util.List;
+import java.util.Map;
+
+public class Day2 implements Day {
+
+    String fileName = "day2.txt";
+
+    @Override
+    public String getAnswer(Part part) {
+        List<String> lines = FilesUtil.getLines(fileName);
+        Integer scoreTotal = 0;
+        for (String line : lines) {
+            Integer score = getScore(line, part);
+            scoreTotal += score;
+        }
+        return String.valueOf(scoreTotal);
+    }
+
+
+    private Integer getScore(String line, Part part) {
+        int score = 0;
+        Map<String, Choice> choiceMapping = Map.of("A", Choice.ROCK, "B", Choice.PAPER, "C", Choice.SCISSOR, "X", Choice.ROCK, "Y", Choice.PAPER, "Z", Choice.SCISSOR);
+        String[] values = line.split(" ");
+        Choice myChoice = null;
+        Choice theirChoice = choiceMapping.get(values[0]);
+        if (part == Part.ONE) {
+            choiceMapping = Map.of("A", Choice.ROCK, "B", Choice.PAPER, "C", Choice.SCISSOR, "X", Choice.ROCK, "Y", Choice.PAPER, "Z", Choice.SCISSOR);
+            myChoice = choiceMapping.get(values[1]);
+        } else if (part == Part.TWO) {
+            myChoice = getMyChoice(theirChoice, values[1]);
+        }
+        score = getChoicePoints(myChoice) + getResultPoints(myChoice, theirChoice);
+        return score;
+    }
+
+    private Choice getMyChoice(Choice theirChoice, String desiredResultString) {
+        Map<String, Result> desiredResultMap = Map.of("X", Result.LOSE, "Y", Result.DRAW, "Z", Result.WIN);
+        Result desiredResult = desiredResultMap.get(desiredResultString);
+        if (desiredResult == Result.DRAW) {
+            return theirChoice;
+        }
+        if (theirChoice == Choice.ROCK) {
+            if (desiredResult == Result.LOSE) {
+                return Choice.SCISSOR;
+            } else if (desiredResult == Result.WIN) {
+                return Choice.PAPER;
+            }
+        } else if (theirChoice == Choice.PAPER) {
+            if (desiredResult == Result.LOSE) {
+                return Choice.ROCK;
+            } else if (desiredResult == Result.WIN) {
+                return Choice.SCISSOR;
+            }
+        } else if (theirChoice == Choice.SCISSOR) {
+            if (desiredResult == Result.LOSE) {
+                return Choice.PAPER;
+            } else if (desiredResult == Result.WIN) {
+                return Choice.ROCK;
+            }
+        }
+        return null;
+    }
+
+    private Integer getChoicePoints(Choice myChoice) {
+        Map<Choice, Integer> pointsMap = Map.of(Choice.ROCK, 1, Choice.PAPER, 2, Choice.SCISSOR, 3);
+        return pointsMap.get(myChoice);
+    }
+
+    private Integer getResultPoints(Choice myChoice, Choice theirChoice) {
+        if (myChoice == theirChoice) {
+            // DRAW
+            return 3;
+        } else if (
+            (myChoice == Choice.ROCK && theirChoice == Choice.SCISSOR) ||
+            (myChoice == Choice.PAPER && theirChoice == Choice.ROCK) ||
+            (myChoice == Choice.SCISSOR && theirChoice == Choice.PAPER)
+        ) {
+            // WIN
+            return 6;
+        } else {
+            // LOSS
+            return 0;
+        }
+    }
+
+    enum Choice {
+        ROCK,
+        PAPER,
+        SCISSOR
+    }
+
+    enum Result {
+        LOSE,
+        DRAW,
+        WIN
+    }
+}
